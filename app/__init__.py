26b0aa01c27f386ab7bb0ed49363e5d7d9443f47
Anna <ann.kh18@gmail.com>
2025-03-17 19:01:59 +0200

    Updated  Flask configuration in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-from app import routes 
+# Підключення до MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:h5s%4L%PeYXC@localhost/pharmacy_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models  # Імпортуємо маршрути та моделі
