6b2eeb9b7b3f981918e4546145c5ae6f3b659c5e
Anna <ann.kh18@gmail.com>
2025-04-01 18:15:44 +0300

    Configured Flask secret key for session support

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ import pymysql
 
 pymysql.install_as_MySQLdb()
 
+
 app = Flask(__name__, template_folder='../templates')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:h5s%4L%PeYXC@localhost/pharmacy_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secret_key = 'super-secret-kit' 
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
