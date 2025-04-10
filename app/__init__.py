987a68e4ba48a5a20aac511433077cfd21732d5e
Anna <ann.kh18@gmail.com>
2025-04-10 20:45:48 +0300

    Fix app initialization and configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,40 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import pymysql
+import os
+from flask_mail import Mail
+from app.config import Config 
 
 pymysql.install_as_MySQLdb()
 
+# Завантажуємо змінні середовища з .env
 
+
+# Ініціалізація Flask додатку
 app = Flask(__name__, template_folder='../templates')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:h5s%4L%PeYXC@localhost/pharmacy_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Налаштування конфігурації через клас Config
+app.config.from_object(Config)
 
-app.secret_key = 'super-secret-kit' 
+# Ініціалізація Mail
+mail = Mail(app)
 
+# Ініціалізація SQLAlchemy
 db = SQLAlchemy(app)
+
+# Ініціалізація міграцій
 migrate = Migrate(app, db)
+
+def create_app():
+    return app
 
 def register_routes():
     from app import routes
 
 register_routes()
+
+print(os.getenv('SQLALCHEMY_DATABASE_URI'))
